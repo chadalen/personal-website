1098d4d52bdd3d53a760e96882453e91f6a53480
chadalen <nshusa99@gmail.com>
2019-06-24 17:07:23 -0500

    Added site metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,44 @@
-import React from "react"
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Container, Button } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Helmet } from "react-helmet";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Button
+} from "@material-ui/core";
 import { Link } from "gatsby";
 
 const styles = makeStyles({
-    root: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1
+  }
 });
 
 export default ({ children }) => {
-    const classes = styles()
-    return (
-        <div className={classes.root}>
-            <AppBar position="static" color="default">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" className={classes.title}>
-                        Chad Adams
-                </Typography>
-                    <Link to="/" style={{textDecoration: 'none', color: 'black'}}>
-                        <Button color="inherit">Blogs</Button>
-                    </Link>
-
-
-                </Toolbar>
-            </AppBar>
-            <Container style={{ marginTop: '5px' }} maxWidth="md">
-                {children}
-            </Container>
-        </div>
-    )
-}
+  const classes = styles();
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Chad Adams</title>
+      </Helmet>
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.title}>
+              Chad Adams
+            </Typography>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              <Button color="inherit">Blogs</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Container style={{ marginTop: "5px" }} maxWidth="md">
+          {children}
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import Layout from "../components/layout";
 
@@ -8,43 +8,40 @@ const styles = makeStyles({
   date: {
     fontSize: 14
   }
-})
+});
 
 export default ({ data }) => {
   console.log(data);
-  const classes = styles()
+  const classes = styles();
   return (
     <Layout>
-        {data.allMarkdownRemark.edges.map(({ node }, index) => (
+      {data.allMarkdownRemark.edges.map(({ node }, index) => (
+        <Card key={index} style={{ marginBottom: "5px" }}>
+          <CardContent>
+            <Link
+              to={node.fields.slug}
+              style={{
+                textDecoration: "none",
+                color: "inherit"
+              }}
+            >
+              <Typography variant="h4">{node.frontmatter.title}</Typography>
+            </Link>
 
-          <Card key={index} style={{ marginBottom: '5px' }}>
-
-            <CardContent>
-
-              <Link
-                to={node.fields.slug}
-                style={{
-                  textDecoration: 'none',
-                  color: 'inherit'
-                }}
-              >
-                <Typography variant="h4">
-                  {node.frontmatter.title}
-                </Typography>
-
-              </Link>
-
-              <Typography className={classes.date} color="textSecondary" gutterBottom>
+            <Typography
+              className={classes.date}
+              color="textSecondary"
+              gutterBottom
+            >
               {node.frontmatter.date}
-              </Typography>
+            </Typography>
 
-              <Typography variant="body2" color="textSecondary" component="p">
-                {node.excerpt}
-        </Typography>
-            </CardContent>
-          </Card>
-
-        ))}
+            <Typography variant="body2" color="textSecondary" component="p">
+              {node.excerpt}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
     </Layout>
   );
 };
