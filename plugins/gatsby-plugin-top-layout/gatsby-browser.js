98e0882c2706abfac7d3fb861c81aed5c837cd65
chadalen <nshusa99@gmail.com>
2019-06-26 21:56:30 -0500

    Add global css

--- plugins/gatsby-plugin-top-layout/gatsby-browser.js
+++ plugins/gatsby-plugin-top-layout/gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TopLayout from './TopLayout';
+import "./src/styles/global.css"
 
 export const wrapRootElement = ({ element }) => {
   return <TopLayout>{element}</TopLayout>;
