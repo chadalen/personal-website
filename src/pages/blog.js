c3bc673af98e00008534ee5b5423e439f5816f44
Chad Adams <nshusa99@gmail.com>
2019-11-22 00:17:41 -0600

    Change blogs page layout and look

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Divider, Typography } from "@material-ui/core";
 import Layout from "../components/layout";
 
 const styles = makeStyles({
   date: {
-    fontSize: '16px'
+    fontSize: "16px"
   },
   summary: {
-    fontSize: '20px'
+    fontSize: "20px"
+  },
+  link: {
+    color: '#337ab7',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#23527c',
+      textDecoration: 'underline'
+    }
   }
 });
 
@@ -18,41 +26,40 @@ export default ({ data }) => {
   const classes = styles();
   return (
     <Layout>
+      <Typography variant="h3" style={{marginTop: '20px', marginBottom: '20px'}}>Blog</Typography>
+      <Divider />
       {data.allMarkdownRemark.edges.map(({ node }, index) => (
-        <Card key={index} style={{ marginBottom: "5px" }}>
-          <CardContent>
-            <div>
-              <img
-                style={{ width: "64px", position: "relative", marginRight: "10px" }}
-                src="./avatar.png"
-                alt="Avatar"
-              />
+        <React.Fragment>
+          <div style={{padding: '20px'}}>
+          <Link
+            to={node.fields.slug}
+            className={classes.link}
+          >
+            <Typography variant="h4">{node.frontmatter.title}</Typography>
+          </Link>
 
-              <div style={{display: "inline-block", position: "absolute"}}>
-              <Link
-                to={node.fields.slug}
-                style={{
-                  textDecoration: "none",
-                  color: "inherit"
-                }}
-              >
-                <Typography variant="h4">{node.frontmatter.title}</Typography>
-              </Link>
-              <Typography
-                  className={classes.date}
-                  color="textSecondary"
-                  gutterBottom
-                >
-                  {node.frontmatter.date}
-                </Typography>
-              </div>
-            </div>
+          <Typography
+            className={classes.date}
+            color="textSecondary"
+            gutterBottom
+          >
+            {node.frontmatter.date}
+          </Typography>
 
-            <Typography className={classes.summary} color="textSecondary" component="p">
-              {node.excerpt}
-            </Typography>
-          </CardContent>
-        </Card>
+          <Typography
+            className={classes.summary}
+            component="p"
+          >
+            {node.excerpt}
+          </Typography>
+
+          <Link
+          to={node.fields.slug}
+          className={classes.link}>
+          <Typography component="p">Read More...</Typography>
+          </Link>
+          </div>
+        </React.Fragment>
       ))}
     </Layout>
   );
