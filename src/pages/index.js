99a78b0598bd9339473a89b0c7986eb24ddafcdb
Chad Adams <nshusa99@gmail.com>
2019-11-21 23:08:19 -0600

    Add basic content to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../components/layout";
+import { makeStyles } from "@material-ui/core/styles";
+
+const styles = makeStyles({
+  wrapper: {
+    height: '75vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  text: {
+    fontSize: '32pt',
+    lineHeight: '36pt',
+    whiteSpace: 'pre'
+  },
+  highlight: {
+    color: '#E31B6D'
+  }
+});
 
 export default () => {
+  const classes = styles();
   return (
     <Layout>
-      <p>Home page</p>
+      <div className={classes.wrapper}>
+        <div className={classes.text}>Hello, I'm <span className={classes.highlight}>Chad Adams</span>.
+        </div>
+        <div className={classes.text}>
+          I'm a full-stack web developer.
+        </div>
+      </div>
     </Layout>
   );
 };
