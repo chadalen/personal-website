e8aa434fbd437e258636fef2e256fa7f38ac1ae8
chadalen <me@chadalen.com>
2020-12-28 15:08:25 -0500

    Created a Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <>
+      <div className="text-center my-4">
+        Copyright © Chad Adams 2016-2021 | All rights reserved
+      </div>
+    </>
+  );
+};
