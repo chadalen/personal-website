5c21544c08ef55180c6b2854cdc2fb0009bec65e
chadalen <me@chadalen.com>
2020-12-28 15:09:09 -0500

    Added Footer component to Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default ({ children }) => {
   return (
@@ -7,7 +8,10 @@ export default ({ children }) => {
       <Navbar />
 
       <div className="grid grid-cols-12" style={{ paddingTop: '64px' }}>
-        <div className="col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3 p-2">{children}</div>
+        <div className="col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3 px-2 pt-2">
+          {children}
+          <Footer />
+        </div>
       </div>
     </>
   );
