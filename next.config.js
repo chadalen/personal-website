ddce296e68cd2f12a77731d251222e0b4e49af50
chadalen <me@chadalen.com>
2021-01-18 00:29:19 -0500

    Improve accessibility on homepage

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   webpack: (cfg) => {
-      cfg.module.rules.push(
-          {
-              test: /\.md$/,
-              loader: 'frontmatter-markdown-loader',
-              options: { mode: ['react-component'] }
-          }
-      )
-      return cfg;
-  }
-}
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    });
+    return cfg;
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+};
