b83c69d0b0d9d202876224adb91268d5c8df44b9
chadalen <me@chadalen.com>
2021-01-17 22:08:18 -0500

    Move css out of Layout component to global styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,27 +4,18 @@ import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
-    <>
-      <div className="flex flex-col min-h-screen bg-white">
-        <Navbar />
+    <div className="flex flex-col min-h-screen bg-white">
+      <Navbar />
 
-        <div className="flex-grow" style={{ paddingTop: '64px' }}>
-          <div className="grid grid-cols-12">
-            <div className="col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3 px-2 pt-2">
-              {children}
-            </div>
+      <div className="flex-grow" style={{ paddingTop: '64px' }}>
+        <div className="grid grid-cols-12">
+          <div className="col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3 px-2 pt-2">
+            {children}
           </div>
         </div>
-
-        <Footer />
       </div>
 
-      <style global jsx>{`
-        html, body, #__next {
-          min-height: 100vh;
-        }
-        `}
-      </style>
-    </>
+      <Footer />
+    </div>
   );
 }
