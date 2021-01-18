3f613b6f7923d04e7021e66e6d5d0451b2b98420
chadalen <me@chadalen.com>
2021-01-18 11:33:41 -0500

    Fix Card component linting

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import clsx from "clsx";
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
 
-export default function Card({ children, className, ...props }) {
+export default function Card({ children, className }) {
   return (
-    <div className={clsx("bg-white p-4 border-b border-gray-200 shadow rounded-lg", className)} {...props}>
+    <div
+      className={clsx(
+        'bg-white p-4 border-b border-gray-200 shadow rounded-lg',
+        className,
+      )}
+    >
       {children}
     </div>
   );
 }
+
+Card.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  className: PropTypes.string,
+};
+
+Card.defaultProps = {
+  className: '',
+};
