90a5cbb2882bc539c4bd6206d2f96549f2906432
chadalen <me@chadalen.com>
2021-01-17 22:05:05 -0500

    Added white bg to content

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 export default function Layout({ children }) {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-white">
         <Navbar />
 
         <div className="flex-grow" style={{ paddingTop: '64px' }}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 
 export default function Page() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-white">
       <Navbar />
       <div
         id="intro"
