976dd8bf4ded73fc3793f1f8bf8eb2dbd4dc9329
chadalen <me@chadalen.com>
2021-01-18 11:36:40 -0500

    Fix DateFormatter linting

--- src/components/DateFormatter.jsx
+++ src/components/DateFormatter.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { parseISO, format } from 'date-fns';
+import PropTypes from 'prop-types';
 
-export default function DateFormatter({ dateString, formatString, ...props }) {
-  const date = parseISO(dateString)
-  const formattedDateString = format(date, formatString)
-  return (
-    <time {...props}>
-      {formattedDateString}
-    </time>
-  )
+export default function DateFormatter({ dateString, formatString, className }) {
+  const date = parseISO(dateString);
+  const formattedDateString = format(date, formatString);
+  return <time className={className}>{formattedDateString}</time>;
 }
+
+DateFormatter.propTypes = {
+  dateString: PropTypes.string.isRequired,
+  formatString: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+DateFormatter.defaultProps = {
+  className: '',
+};
