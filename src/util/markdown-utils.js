f9b295ca81e8be15bba425d4501ba633dfbf46b3
chadalen <me@chadalen.com>
2021-01-26 22:30:08 -0500

    Force links in markdown to open in a new tab

--- src/util/markdown-utils.js
+++ src/util/markdown-utils.js
@@ -1,7 +1,8 @@
-import remark from 'remark'
-import html from 'remark-html'
+import remark from 'remark';
+import html from 'remark-html';
+import externalLinks from 'remark-external-links';
 
 export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const result = await remark().use(externalLinks, { target: '_blank' }).use(html).process(markdown);
+  return result.toString();
 }
